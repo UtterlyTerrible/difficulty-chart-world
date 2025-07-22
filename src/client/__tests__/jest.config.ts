fc2a473af293a6a4e0f915842cc1d6c05590c44e
UtterlyTerrible <hoarsekat@outlook.com>
2025-07-22 20:08:55 +1000

    🧪 Add client and server jest tests

---
+++ src/client/__tests__/jest.config.ts
@@ -0,0 +1,7 @@
+import { Config } from "@rbxts/jest";
+
+export = {
+	displayName: "🔵 CLIENT",
+	testMatch: ["**/*.spec"],
+// eslint-disable-next-line prettier/prettier
+} satisfies Config;
---
+++ src/server/__tests__/jest.config.ts
@@ -0,0 +1,7 @@
+import { Config } from "@rbxts/jest";
+
+export = {
+	displayName: "🟢 SERVER",
+	testMatch: ["**/*.spec"],
+// eslint-disable-next-line prettier/prettier
+} satisfies Config;
